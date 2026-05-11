868a8482fc87d8e6ff2e185489d21221825abc9b
AleXxxj <152504253+AleXxxj@users.noreply.github.com>
2026-05-11 19:10:34 +0100

    Adjust bottom position of nav-float-pill

--- theme.js
+++ theme.js
@@ -228,7 +228,7 @@
 /* ══ Floating pill (nav open state) ══════════════ */
 .nav-float-pill {
     position: fixed;
-    bottom: 275px;
+    bottom: 279px;
     right: 16px;
     display: flex;
     flex-direction: row;
